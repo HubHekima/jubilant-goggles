e227db3013d9173a757e2e8d6adcba4fa482b77c
HubHekima <hubhekima@gmail.com>
2026-05-10 14:00:42 +0300

    Reverting to stable version 8eba58a

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,6 @@ Route::middleware(['auth', 'role:super-admin'])->prefix('admin')->group(function
     Route::get('/events/create', CreateEvent::class)->name('admin.events.create');
 });
 
-Route::get('/scanner', TicketScanner::class)->name('scanner');
+Route::get('/scanner', TicketScanner::class);
 
 require __DIR__.'/settings.php';
