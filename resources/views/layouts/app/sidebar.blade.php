bc56f67d438d5c0a252d867b6459bc84f7f7a655
HubHekima <hubhekima@gmail.com>
2026-05-10 15:55:29 +0300

    changes

--- resources/views/layouts/app/sidebar.blade.php
+++ resources/views/layouts/app/sidebar.blade.php
@@ -16,13 +16,14 @@
                         {{ __('Dashboard') }}
                     </flux:sidebar.item>
                     @can('manage events')
-                    <flux:sidebar.item icon="home" wire:navigate>
-                        {{ __('New Event') }}
+                    <flux:sidebar.item icon="calendar-plus"
+                    :href="route('admin.events.create')" :current="request()->routeIs('admin.events.create')" wire:navigate>
+                        {{ __('Create Event') }}
                     </flux:sidebar.item>
                     @endcan
                     @can('scan tickets')
-                    <flux:sidebar.item icon="home" wire:navigate>
-                        {{ __('Gate Scanner') }}
+                    <flux:sidebar.item icon="qr-code"  :href="route('scanner')" :current="request()->routeIs('scanner')" wire:navigate>
+                        {{ __('Scan Ticket') }}
                     </flux:sidebar.item>
                     @endcan
                 </flux:sidebar.group>
